7ab2a72af05f1f805708142c46ad400fe31013ae
Matt Ha <matthewha1993@gmail.com>
2025-09-30 18:13:35 -0600

    Empty state for columns

--- src/components/TaskColumn.tsx
+++ src/components/TaskColumn.tsx
@@ -15,21 +15,25 @@ export function TaskColumn({
   onDelete,
 }: TaskColumnProps) {
   return (
-    <div className="flex-1 p-4 border rounded-md shadow-md min-w-[250px] max-w-[300px] flex flex-col">
+    <div className="flex-1 p-4 border rounded-md shadow-md min-w-[250px] max-w-[300px] flex flex-col min-h-[750px]">
       <h2 className="font-semibold mb-2 text-center border-b pb-4 mb-4">
         {title}
       </h2>
       <ul className="flex-1 overflow-y-auto space-y-2">
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <TaskCard
-              task={task}
-              onMovePrevious={(id) => moveTask(id, "previous")}
-              onMoveNext={(id) => moveTask(id, "next")}
-              onDelete={onDelete}
-            />
-          </li>
-        ))}
+        {tasks.length === 0 ? (
+          <li className="text-gray-500 text-center mt-4">No tasks</li>
+        ) : (
+          tasks.map((task) => (
+            <li key={task.id}>
+              <TaskCard
+                task={task}
+                onMovePrevious={(id) => moveTask(id, "previous")}
+                onMoveNext={(id) => moveTask(id, "next")}
+                onDelete={onDelete}
+              />
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
